52470d1764f65b0bb2c54fa4aafc15cbc7a700b8
MReyesSegura <marioreyesdeveloper@gmail.com>
2024-01-24 13:12:16 -0600

    Se agregan componentes para MVP y vista inicial de home

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { UtilService } from 'src/app/services/util.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,21 @@ import { UtilService } from 'src/app/services/util.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  user: string = ''
+  password: string = ''
 
   constructor(
   private util: UtilService,
-  private navCtrl: NavController
+  private navCtrl: NavController,
+  private router: Router
   ) { }
 
   ngOnInit() {
   }
+
+doLogin() {
+this.router.navigate(['home'])
+}  
 
 back() {
   this.navCtrl.back()
