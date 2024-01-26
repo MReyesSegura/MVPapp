a3e8969e44ef33429dc6a69af4bd97cbe36b7abd
MReyesSegura <marioreyesdeveloper@gmail.com>
2024-01-26 09:11:26 -0600

    Se agrega manejo de errores a el modulo 'mi perfil'

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -10,6 +10,11 @@ import { UtilService } from 'src/app/services/util.service'
 export class ProfilePage implements OnInit {
     language: string = 'Español'
     cfdi: string = 'false'
+    email: string = 'mario.reyes@lklpay.com.mx'
+    rfc: string = 'RESM090909UOI'
+    social: string = 'LKLPAY SA de CV'
+    password: string = ''
+    passwordC: string = ''
     profileActive: boolean = true
     passwordActive: boolean = false
 
@@ -25,6 +30,56 @@ export class ProfilePage implements OnInit {
     goToProfile() {
         this.profileActive = true
         this.passwordActive = false
+    }
+
+    saveProfileinformation() {
+        if (!this.util.isValidEmail(this.email)) {
+            this.util.showToast('Correo invalido', 'danger')
+            return
+        }
+
+        if (
+            this.language != null &&
+            this.language !== '' &&
+            this.cfdi != null &&
+            this.cfdi !== '' &&
+            this.email != null &&
+            this.email !== '' &&
+            this.rfc != null &&
+            this.rfc !== '' &&
+            this.social != null &&
+            this.social !== ''
+        ) {
+            this.util.showToast(
+                'La información fue actualizada de manera exitosa',
+                'success'
+            )
+        } else {
+            this.util.showToast(
+                'Completa todos los campos para actualizar la información',
+                'danger'
+            )
+        }
+    }
+
+    savePasswordInformation() {
+        if (this.password !== this.passwordC) {
+            this.util.showToast('Las contraseñas deben ser identicas', 'danger')
+            return
+        }
+
+        if (!this.util.isValidPassword(this.password)) {
+            this.util.showToast(
+                'Agrega una contraseña con el formato correcto',
+                'danger'
+            )
+            return
+        }
+
+        this.util.showToast(
+            'La contraseña fue actualizada correctamente',
+            'success'
+        )
     }
 
     back() {
